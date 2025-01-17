3d01bb4f66c0fbae9d8e0d1525b4f0259592cd5b
Sai-Srevan2004 <22bd1a057l@gmail.com>
2025-01-17 16:19:01 +0530

    sample maven java

---
+++ src/main/java/saisrevan/SampleMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package saisrevan.SampleMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
